83a9157b479c8922d8476cd9564e32a27b63fec6
coderhally <coderresnick7984@gmail.com>
2023-04-28 15:57:03 +0530

    Created Main Menu

---
+++ Tapash/Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitButton()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
